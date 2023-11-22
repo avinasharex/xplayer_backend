935a4d5505ee784c5d779bf12b6da103db055863
avinasharex <avinash462470@gmail.com>
2023-11-22 10:19:57 +0530

    controllers and routes setup configured

--- src/app.js
+++ src/app.js
@@ -12,5 +12,10 @@ app.use(express.urlencoded({extended: true, limit: "20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes import
+import userRouter from './routes/user.route.js'
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
 
 export{app}
