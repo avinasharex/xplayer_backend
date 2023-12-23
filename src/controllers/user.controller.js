b7708b3b029ddb81c77d3241c7f46c1d90ccb839
avinasharex <avinash462470@gmail.com>
2023-12-23 13:39:37 +0530

    Write get user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -237,10 +237,17 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully"));
 });
 
+const getCurrentUser = asyncHandler(async(req,res)=>{
+  return res
+  .status(200)
+  .json(200, req.user, "current user fetched successfully")
+})
+
 export {
   registerUser,
   loginUser,
   logoutUser,
   refresAccessToken,
   changeCurrentPassword,
+  getCurrentUser
 };
