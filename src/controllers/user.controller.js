e4d019a89ccccefce934ce123d3eba281cee6e2b
avinasharex <avinash462470@gmail.com>
2023-12-24 07:43:59 +0530

    Write update account details controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -243,11 +243,32 @@ const getCurrentUser = asyncHandler(async(req,res)=>{
   .json(200, req.user, "current user fetched successfully")
 })
 
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+  const {fullName,email} = req.body
+  if(!(fullName || email)){
+    throw new ApiError(400, "All fields are required")
+  }
+
+  const user = await User.findByIdAndUpdate(req.user._id,
+    {
+      $set:{
+        fullName,
+        email
+      }
+    },
+    {new: true}).select("-password")
+
+    return res 
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+})
+
 export {
   registerUser,
   loginUser,
   logoutUser,
   refresAccessToken,
   changeCurrentPassword,
-  getCurrentUser
+  getCurrentUser,
+  updateAccountDetails
 };
