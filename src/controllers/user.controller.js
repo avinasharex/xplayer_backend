1ae36ca2bc4e4086a01e0dc624416aa2626e2279
avinasharex <avinash462470@gmail.com>
2023-12-25 09:59:52 +0530

    Write update user cover image controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -283,6 +283,40 @@ const updateUserAvatar = asyncHandler(async(req,res)=>{
         avatar: avatar.url
       }
     },{new: true}).select("-password")
+
+    return res 
+    .status(200)
+    .json(
+      new ApiResponse(200, user, "Avatar image updated successfully")
+    )
+})
+
+const updateUserCoverImage = asyncHandler(async(req,res)=>{
+
+  const coverImageLocalPath = req.file?.path
+
+  if(!coverImageLocalPath){
+    throw new ApiError(400, "Cover image file is missing")
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if(!coverImage.url){
+    throw new ApiError(400, "Error while uploading avatar")
+  }
+
+  const user = await User.findByIdAndUpdate(req.user?._id,
+    {
+      $set:{
+        coverImage: coverImage.url
+      }
+    },{new: true}).select("-password")
+
+    return res 
+    .status(200)
+    .json(
+      new ApiResponse(200, user, "Cover image updated successfully")
+    )
 })
 
 export {
@@ -293,5 +327,6 @@ export {
   changeCurrentPassword,
   getCurrentUser,
   updateAccountDetails,
-  updateUserAvatar
+  updateUserAvatar,
+  updateUserCoverImage
 };
